4f36e889e051fac6aa2930d42e0771b8d152e34b
koseki0830 <abcdg651@gmail.com>
2023-09-11 14:31:53 +0900

    update: カテゴリー登録機能

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,6 @@ class MoviesController < ApplicationController
 
   def create
     @movie = current_user.movies.new(movie_params)
-
     if @movie.save
       redirect_to movies_path
     else
@@ -45,6 +44,6 @@ class MoviesController < ApplicationController
   end
 
   def movie_params
-    params.require(:movie).permit(:title, :synopsis, :thumbnail, :thumbnail_cache, :screening_time)
+    params.require(:movie).permit(:title, :synopsis, :thumbnail, :thumbnail_cache, :screening_time, category_ids: [])
   end
 end

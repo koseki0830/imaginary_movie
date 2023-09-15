363523a65c1279ce4e3322e75c5f297c6944082e
koseki0830 <abcdg651@gmail.com>
2023-09-15 15:23:30 +0900

    feat: レビューの新規登録画面をモーダル化

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "modal" ]
+  connect() {
+  }
+
+  open() {
+    this.modalTarget.classList.remove('hidden');
+  }
+
+  close() {
+    this.modalTarget.classList.add('hidden');
+  }
+
+  // モーダルをとじる
+  closeModal() {
+    this.close()
+  }
+}

5ce80674bbc4404ccd1aa59dc0b53a417748a289
koseki0830 <abcdg651@gmail.com>
2023-10-02 16:18:01 +0900

    feat: いいねのロゴ表示、非同期通信実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,10 @@ class LikesController < ApplicationController
   def create 
     review = Review.find(params[:review_id])
     current_user.like(review)
-    redirect_back fallback_location: root_path
   end 
   
   def destroy
     review = current_user.likes.find(params[:id]).review
     current_user.unlike(review)
-    redirect_back fallback_location: root_path
   end
 end

23dc8aa403566a5ac60bd9a460c613aa58e2912e
koseki0830 <abcdg651@gmail.com>
2023-10-07 20:08:58 +0900

    feat: movies/bookmarks.html.erb作成

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -38,12 +38,15 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  # それぞれのサイドバーに、ブックマークした映画とレビューした映画数を表示するため変数を定義
   def my_reviews_movies
+    @bookmarks_movies = current_user.bookmark_movies.includes(:user).order(created_at: :desc)
     @movies = current_user.reviews.includes(:movie).map(&:movie)
   end
 
   def bookmarks
     @bookmarks_movies = current_user.bookmark_movies.includes(:user).order(created_at: :desc)
+    @movies = current_user.reviews.includes(:movie)
   end
 
   private

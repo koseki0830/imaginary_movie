d2aab833d2c7336c20533a4d441e3b4fb41b796a
koseki0830 <abcdg651@gmail.com>
2023-10-07 19:26:15 +0900

    feat: ブックマークボタン作成・ajax化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,14 +1,12 @@
 class BookmarksController < ApplicationController
   
   def create
-    movie = Movie.find(params[:movie_id])
-    current_user.bookmark(movie)
-    redirect_back fallback_location: root_path
+    @movie = Movie.find(params[:movie_id])
+    current_user.bookmark(@movie)
   end
 
   def destroy
-    movie = current_user.bookmarks.find(params[:id]).movie
-    current_user.unbookmark(movie)
-    redirect_back fallback_location: root_path
+    @movie = current_user.bookmarks.find(params[:id]).movie
+    current_user.unbookmark(@movie)
   end
 end

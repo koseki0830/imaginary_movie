d2b7e449e63bda00c081bd7dfe1d95ab2eee76d0
koseki0830 <abcdg651@gmail.com>
2023-10-12 13:36:12 +0900

    update: レビュー一覧と詳細でレビュー数表示

---
+++ app/helpers/reviews_helper.rb
@@ -0,0 +1,12 @@
+module ReviewsHelper
+  def getPercent(number) 
+    if number.present?
+      calPercent = number/5.to_f * 100
+      percent = calPercent.round
+      #↑CSSは小数が含まれると、widthの幅を指定できないので四捨五入して整数化
+      return percent.to_s
+    else
+      return 0
+    end
+  end
+end

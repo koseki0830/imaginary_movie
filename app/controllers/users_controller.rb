0e37ef6e47003b872b9826778ff2cb7bb071f7bf
koseki0830 <abcdg651@gmail.com>
2023-10-15 20:31:46 +0900

    update: 会員登録時のフラッシュメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path # ユーザー作成後にログインするよう変更予定
+      flash[:notice] = "ユーザー登録が完了しました!"
+      redirect_to login_path 
     else
       render :new
     end

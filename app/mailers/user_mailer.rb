19e6ceb357237d1bb578fd2f2f22e102e92b2f19
koseki0830 <abcdg651@gmail.com>
2023-10-17 11:53:26 +0900

    feat: メイラーの設定

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  default from: 'from@example.com'
+  def reset_password_email
+    @user = User.find(user.id)
+    @url = edit_password_reset_url(@user.reset_passoword_token)
+
+    mail(to: user.email, subject: 'パスワードリセット')
+  end
+end

62ca0d17174e7894289e53f8c1791644017b0810
koseki0830 <abcdg651@gmail.com>
2023-11-09 14:49:50 +0900

    update: ページネーションを各viewに表示

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -43,15 +43,12 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
-  # それぞれのサイドバーに、ブックマークした映画とレビューした映画数を表示するため変数を定義
   def my_reviews_movies
-    @bookmarks_movies = current_user.bookmark_movies.includes(:user).order(created_at: :desc)
-    @movies = current_user.reviews.includes(:movie).map(&:movie)
+    @reviews_movies = current_user.reviews.includes(:movie).order(created_at: :desc).page(params[:page])
   end
 
   def bookmarks
-    @bookmarks_movies = current_user.bookmark_movies.includes(:user).order(created_at: :desc)
-    @movies = current_user.reviews.includes(:movie)
+    @bookmarks_movies = current_user.bookmark_movies.includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   private

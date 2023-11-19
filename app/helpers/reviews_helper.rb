3a3bd38e790a95f3b1890c34dcec6e8cddcf611b
koseki0830 <abcdg651@gmail.com>
2023-11-19 21:22:50 +0900

    test: reviewsスペック

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -3,7 +3,7 @@ module ReviewsHelper
     if number.present?
       calPercent = number/5.to_f * 100
       percent = calPercent.round
-      #↑CSSは小数が含まれると、widthの幅を指定できないので四捨五入して整数化
+      #CSSは小数が含まれると、widthの幅を指定できないので四捨五入して整数化
       return percent.to_s
     else
       return 0

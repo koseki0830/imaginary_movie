c56ca7c5fc1930ae51eb8c0ca7582b8128ce22d0
koseki0830 <abcdg651@gmail.com>
2023-11-24 15:04:10 +0900

    update: config/initializers/carrierwave.rbの編集

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,11 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
+  if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'imaginary-movie'
+    config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
@@ -13,4 +15,8 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1',
       path_style: true
     }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end 

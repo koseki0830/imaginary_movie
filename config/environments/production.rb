65551d9f67487a59857c5beaa4efa0376d2022f4
koseki0830 <abcdg651@gmail.com>
2023-12-03 17:36:34 +0900

    update: パスワードリセット機能を本番用に設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = Settings.default_url_options.to_h
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: ENV['GMAIL_ADDRESS'], 
+    password: ENV['GMAIL_PASSWORD'], 
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end

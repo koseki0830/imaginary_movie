52331bcbbf5f0f38bc98492eafc56a28ce72adcb
koseki0830 <abcdg651@gmail.com>
2023-12-13 23:52:47 +0900

    update: usersテーブルにadminカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,107 +10,108 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_231_112_084_144) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_13_144803) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'bookmarks', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'movie_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['movie_id'], name: 'index_bookmarks_on_movie_id'
-    t.index %w[user_id movie_id], name: 'index_bookmarks_on_user_id_and_movie_id', unique: true
-    t.index ['user_id'], name: 'index_bookmarks_on_user_id'
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "movie_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
+    t.index ["user_id", "movie_id"], name: "index_bookmarks_on_user_id_and_movie_id", unique: true
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'comments', force: :cascade do |t|
-    t.text 'content', null: false
-    t.bigint 'user_id', null: false
-    t.bigint 'review_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['review_id'], name: 'index_comments_on_review_id'
-    t.index ['user_id'], name: 'index_comments_on_user_id'
+  create_table "comments", force: :cascade do |t|
+    t.text "content", null: false
+    t.bigint "user_id", null: false
+    t.bigint "review_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["review_id"], name: "index_comments_on_review_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table 'likes', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.bigint 'review_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['review_id'], name: 'index_likes_on_review_id'
-    t.index %w[user_id review_id], name: 'index_likes_on_user_id_and_review_id', unique: true
-    t.index ['user_id'], name: 'index_likes_on_user_id'
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "review_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["review_id"], name: "index_likes_on_review_id"
+    t.index ["user_id", "review_id"], name: "index_likes_on_user_id_and_review_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table 'movie_categories', force: :cascade do |t|
-    t.bigint 'movie_id', null: false
-    t.bigint 'category_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['category_id'], name: 'index_movie_categories_on_category_id'
-    t.index ['movie_id'], name: 'index_movie_categories_on_movie_id'
+  create_table "movie_categories", force: :cascade do |t|
+    t.bigint "movie_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_movie_categories_on_category_id"
+    t.index ["movie_id"], name: "index_movie_categories_on_movie_id"
   end
 
-  create_table 'movies', force: :cascade do |t|
-    t.string 'title', null: false
-    t.text 'synopsis', null: false
-    t.string 'thumbnail'
-    t.integer 'screening_time', null: false
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'text_position'
-    t.string 'text_color'
-    t.string 'text_size'
-    t.string 'font_type'
-    t.index ['user_id'], name: 'index_movies_on_user_id'
+  create_table "movies", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "synopsis", null: false
+    t.string "thumbnail"
+    t.integer "screening_time", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "text_position"
+    t.string "text_color"
+    t.string "text_size"
+    t.string "font_type"
+    t.index ["user_id"], name: "index_movies_on_user_id"
   end
 
-  create_table 'reviews', force: :cascade do |t|
-    t.bigint 'movie_id', null: false
-    t.bigint 'user_id', null: false
-    t.text 'content'
-    t.boolean 'contains_spoiler', default: false, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'rating', default: 0, null: false
-    t.index ['movie_id'], name: 'index_reviews_on_movie_id'
-    t.index ['user_id'], name: 'index_reviews_on_user_id'
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "movie_id", null: false
+    t.bigint "user_id", null: false
+    t.text "content"
+    t.boolean "contains_spoiler", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "rating", default: 0, null: false
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'email', null: false
-    t.string 'crypted_password'
-    t.string 'salt'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_token_expires_at'
-    t.datetime 'reset_password_email_sent_at'
-    t.integer 'access_count_to_reset_password_page', default: 0
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['name'], name: 'index_users_on_name', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token'
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
+    t.boolean "admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
-  add_foreign_key 'bookmarks', 'movies'
-  add_foreign_key 'bookmarks', 'users'
-  add_foreign_key 'comments', 'reviews'
-  add_foreign_key 'comments', 'users'
-  add_foreign_key 'likes', 'reviews'
-  add_foreign_key 'likes', 'users'
-  add_foreign_key 'movie_categories', 'categories'
-  add_foreign_key 'movie_categories', 'movies'
-  add_foreign_key 'movies', 'users'
-  add_foreign_key 'reviews', 'movies'
-  add_foreign_key 'reviews', 'users'
+  add_foreign_key "bookmarks", "movies"
+  add_foreign_key "bookmarks", "users"
+  add_foreign_key "comments", "reviews"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "reviews"
+  add_foreign_key "likes", "users"
+  add_foreign_key "movie_categories", "categories"
+  add_foreign_key "movie_categories", "movies"
+  add_foreign_key "movies", "users"
+  add_foreign_key "reviews", "movies"
+  add_foreign_key "reviews", "users"
 end

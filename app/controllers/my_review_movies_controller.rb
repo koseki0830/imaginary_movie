6d97dce5b8c36a6ebff2ae5c69776dd3e91976d5
koseki0830 <abcdg651@gmail.com>
2023-12-31 12:42:37 +0900

    fix: lintチェック

--- app/controllers/my_review_movies_controller.rb
+++ app/controllers/my_review_movies_controller.rb
@@ -1,10 +1,11 @@
-class MyReviewMoviesController < ApplicationController
+# frozen_string_literal: true
 
+class MyReviewMoviesController < ApplicationController
   def index
     @movies = current_user.reviews
-                                  .joins(:movie)
-                                  .select('distinct on (movies.id) movies.*, reviews.id as review_id, reviews.movie_id')
-                                  .order('movies.id, reviews.created_at desc')
-                                  .page(params[:page])
+                          .joins(:movie)
+                          .select('distinct on (movies.id) movies.*, reviews.id as review_id, reviews.movie_id')
+                          .order('movies.id, reviews.created_at desc')
+                          .page(params[:page])
   end
 end
--- app/controllers/recommends_controller.rb
+++ app/controllers/recommends_controller.rb
@@ -1,5 +1,6 @@
-class RecommendsController < ApplicationController
+# frozen_string_literal: true
 
+class RecommendsController < ApplicationController
   def index
     @movies = current_user.top_categories_movies
   end
